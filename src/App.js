9858a663b06d7be241851c727c5c7d4d608021be
Dev <devpandhi1@gmail.com>
2023-11-05 21:23:18 +0530

    refinements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./component/Home";
 import SocialLinks from "./component/SocialLinks";
 import About from "./component/About";
 import Workfolio from "./component/Workfolio";
+import Experience from "./component/Experience";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <SocialLinks/>
     <About/>
     <Workfolio/>
+    <Experience/>
     </div>
   );
 }
--- src/component/Workfolio.jsx
+++ src/component/Workfolio.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
 import installNode from '../assets/portfolio/installNode.jpg'
 import navbar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/arrayDestruct.jpg'
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
-import reactWeather from '../assets/portfolio/reactWeather.jpg'
-import usestate from '../assets/portfolio/usestate.jpg'
+// import reactParallax from '../assets/portfolio/arrayDestruct.jpg'
+// import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
+// import reactWeather from '../assets/portfolio/reactWeather.jpg'
+// import usestate from '../assets/portfolio/usestate.jpg'
 
 const Workfolio = () => {
 
